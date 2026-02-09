b5a90eb91da445c259834c756a4d2f514c4ee46e
masoudyousefi <masoudyousefi900@gmail.com>
2026-02-09 19:02:11 +0330

    add logo to uploaded image

--- src/shared/services/aws-s3.service.ts
+++ src/shared/services/aws-s3.service.ts
@@ -46,15 +46,53 @@ export class AwsS3Service {
       const key = `${isDevelop ? 'test' : type}/${fileName}`;
       const bucketName = this.configService.awsS3Config.bucketName;
 
-      const quality = type === MediaType.BLOG ? 65 : 75
+      const quality = type === MediaType.BLOG ? 65 : 75;
 
-      const webpBuffer = await sharp(file.buffer)
-        .webp({ quality }) // adjust quality between 1-100
+      const watermarkSvg = `
+      <svg width="1200" height="300" viewBox="0 0 1200 300">
+        <style>
+          .text {
+            fill: rgb(23, 6, 96); /* navy */
+            font-size: 120px;
+            font-family: Arial, sans-serif;
+            letter-spacing: 1px;
+          }
+        </style>
+        <text x="50" y="200" class="text">© AME-TAMA</text>
+      </svg>
+      `;
+      
+
+const thumbnailWatermarkSvg = `
+<svg width="300" height="80">
+  <style>
+    .text {
+      fill: rgba(255,255,255,0.4);
+      font-size: 22px;
+      font-family: Arial, sans-serif;
+    }
+  </style>
+  <text x="0" y="30" class="text">©ame-tama</text>
+</svg>
+`;
+
+      const webpBuffer = await sharp(file.buffer).composite([
+        {
+          input: Buffer.from(watermarkSvg),
+          gravity: 'southwest',
+        },
+      ])
+        .webp({ quality })
         .toBuffer();
 
       // for now because we don't have a good server we must do this for icons
 
-      const thumbnailBuffer = await sharp(file.buffer)
+      const thumbnailBuffer = await sharp(file.buffer).composite([
+        {
+          input: Buffer.from(thumbnailWatermarkSvg),
+          gravity: 'southwest',
+        },
+      ])
         .webp({ quality: 40, preset: 'icon' })
         .toBuffer();
 
@@ -76,6 +114,7 @@ export class AwsS3Service {
 
       return { fileName, type, bucketName };
     } catch (error) {
+        console.log(error);
       return { error };
     }
   }
